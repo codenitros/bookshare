dff0f220d968d612ecde1ffd5212b40c75b1499a
santosh <santoshbhat1998@gmail.com>
2019-10-14 20:40:40 +0530

    db changed to sqlite

--- bookshare/settings.py
+++ bookshare/settings.py
@@ -99,12 +99,8 @@ WSGI_APPLICATION = 'bookshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bookshare',
-        'USER': 'santosh',
-        'PASSWORD': 'mysql7856',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
